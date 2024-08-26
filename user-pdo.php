d0837a31faf85f3c6567dd162c7cc6d58385bb53
alkadabna <abd-ellah.hioun@laplateforme.io>
2024-08-26 14:03:58 +0200

    work

--- user-pdo.php
+++ user-pdo.php
@@ -1,54 +1,58 @@
 <?php
-
-class User {
+class Userpdo {
     private $id;
     public $login;
-    public $password; 
+    public $password; // Added this
     public $email;
     public $firstname;
     public $lastname;
 
+    // Constructor to initialize attributes
     public function __construct($login, $password, $email, $firstname, $lastname, $id = null) {
         $this->id = $id;
         $this->login = $login;
-        $this->password = password_hash($password, PASSWORD_BCRYPT); // Hashing the password
+        $this->password = $password;
         $this->email = $email;
         $this->firstname = $firstname;
         $this->lastname = $lastname;
     }
 
-    public function create($conn) {
+    // Create a new user
+    public function create($pdo) {
         $sql = "INSERT INTO utilisateur (login, password, email, firstname, lastname) VALUES (?, ?, ?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute([$this->login, $this->password, $this->email, $this->firstname, $this->lastname]);
-        
-        if ($stmt) {
-            $this->id = $conn->lastInsertId(); 
-            return true;
-        }
-        return false;
+        $stmt = $pdo->prepare($sql);
+        // Password should be hashed before storing
+        $hashedPassword = password_hash($this->password, PASSWORD_BCRYPT);
+        return $stmt->execute([$this->login, $hashedPassword, $this->email, $this->firstname, $this->lastname]);
     }
 
-    public static function read($conn, $id) {
+    // Read user details by ID
+    public static function read($pdo, $id) {
         $sql = "SELECT * FROM utilisateur WHERE id = ?";
-        $stmt = $conn->prepare($sql);
+        $stmt = $pdo->prepare($sql);
         $stmt->execute([$id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function update($conn) {
+    // Update user details
+    public function update($pdo) {
         $sql = "UPDATE utilisateur SET login = ?, password = ?, email = ?, firstname = ?, lastname = ? WHERE id = ?";
-        $stmt = $conn->prepare($sql);
-        return $stmt->execute([$this->login, $this->password, $this->email, $this->firstname, $this->lastname, $this->id]);
+        $stmt = $pdo->prepare($sql);
+        // Password should be hashed before updating
+        $hashedPassword = password_hash($this->password, PASSWORD_BCRYPT);
+        return $stmt->execute([$this->login, $hashedPassword, $this->email, $this->firstname, $this->lastname, $this->id]);
     }
 
-    public static function delete($conn, $id) {
+    // Delete a user by ID
+    public static function delete($pdo, $id) {
         $sql = "DELETE FROM utilisateur WHERE id = ?";
-        $stmt = $conn->prepare($sql);
+        $stmt = $pdo->prepare($sql);
         return $stmt->execute([$id]);
     }
 
+    // Get ID
     public function getId() {
         return $this->id;
     }
 }
+?>
